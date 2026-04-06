ce2e6f076b8378ba7db3c56512c616af609f5244
Joel Low <joel@joelsplace.sg>
2026-04-06 09:36:27 +0800

    Fork the plugin

--- plugin/init.lua
+++ plugin/init.lua
@@ -11,7 +11,7 @@ local function init()
 	-- enable_sub_modules()
 	local opts = {
 		auto = true,
-		keywords = { "github", "MLFlexer", "resurrect", "wezterm" },
+		keywords = { "github", "lowjoel", "resurrect", "wezterm" },
 	}
 	local plugin_path = dev.setup(opts)
 
